054889296df00d0ef77242216cc2eb12525b6881
浚 刘 <1136647368@qq.com>
2023-08-31 13:56:14 +0800

    添加项目文件

---
+++ CadDemo/DatabaseLearn/Learning1_5.cs
@@ -0,0 +1,81 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:CommandClass(typeof(CadDemo.DatabaseLearn.Learning1_5))]
+namespace CadDemo.DatabaseLearn
+{
+    internal class Learning1_5
+    {
+        /// <summary>
+        /// 写块克隆
+        /// </summary>
+        [CommandMethod("WblockClone")]
+        public void WblockClone()
+        {
+            ObjectIdCollection objectIdCollection = new ObjectIdCollection();
+            foreach (DBObject obj in GetCollection())
+            {
+                objectIdCollection.Add(obj.ObjectId);
+            }
+            WClone(objectIdCollection, "C:\\Users\\AideSoftware\\Desktop\\new.dwg");
+        }
+
+        /// <summary>
+        /// 获取用户选集
+        /// </summary>
+        /// <returns>选择集合</returns>
+        public static DBObjectCollection GetCollection()
+        {
+            Document document = Application.DocumentManager.MdiActiveDocument;
+            Database db = document.Database;
+            Editor editor = document.Editor;
+            DBObjectCollection entityCollection = new DBObjectCollection();
+            PromptSelectionResult promptSelectionResult = editor.GetSelection();
+            if(promptSelectionResult.Status == PromptStatus.OK)
+            {
+                using (Transaction transaction = db.TransactionManager.StartTransaction())
+                {
+                    SelectionSet selectionSet = promptSelectionResult.Value;
+                    foreach(ObjectId id in selectionSet.GetObjectIds())
+                    {
+                        Entity entity = (Entity)transaction.GetObject(id, OpenMode.ForWrite, true); 
+                        if(null != entity)
+                        {
+                            entityCollection.Add(entity);
+                        }
+                    }
+                    transaction.Commit();
+                }
+            }
+            return entityCollection;
+        }
+
+        /// <summary>
+        /// 写块克隆
+        /// </summary>
+        /// <param name="idCollection">要克隆的对象ID集合</param>
+        /// <param name="fileName">克隆到的目标文件名</param>
+        public static void WClone(ObjectIdCollection idCollection,string fileName)
+        {
+            Database newDatabase = new Database(true,true);
+            ObjectId objectId = new ObjectId();
+            Database database = idCollection[0].Database;
+            IdMapping idMapping = new IdMapping(); 
+            using(Transaction transaction = newDatabase.TransactionManager.StartTransaction())
+            {
+                BlockTable blockTable = (BlockTable)transaction.GetObject(newDatabase.BlockTableId, OpenMode.ForRead);
+                BlockTableRecord modelSpace = (BlockTableRecord)transaction.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForRead);
+                objectId = modelSpace.ObjectId;
+                transaction.Commit();
+            }
+            database.WblockCloneObjects(idCollection, objectId, idMapping, DuplicateRecordCloning.Replace, false);
+            newDatabase.SaveAs(fileName, DwgVersion.Current);
+        }
+    }
+}
---
+++ CadDemo/DatabaseLearn/Learning1_6.cs
@@ -0,0 +1,70 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:CommandClass(typeof(CadDemo.DatabaseLearn.Learning1_6))]
+namespace CadDemo.DatabaseLearn
+{
+    internal class Learning1_6
+    {
+        /// <summary>
+        /// 设置当前视图(通过editor的SetCurrentView设置)
+        /// </summary>
+        [CommandMethod("SetCurrentView")]
+        public void SetCurrentView()
+        {
+            Entity entity = SelectEntity("\n请选择一个实体");
+            ViewEntity(entity, 5);
+        }
+
+        /// <summary>
+        /// 选择单个实体
+        /// </summary>
+        /// <param name="message">选择提示</param>
+        /// <returns>实体对象</returns>
+        private Entity SelectEntity(string message)
+        {
+            Document document = Application.DocumentManager.MdiActiveDocument;
+            Database database = document.Database;
+            Editor editor = document.Editor;
+            Entity entity = null;
+            PromptEntityResult result = editor.GetEntity(message);
+            if (result.Status == PromptStatus.OK)
+            {
+                using(Transaction transaction = database.TransactionManager.StartTransaction())
+                {
+                    entity = (Entity)transaction.GetObject(result.ObjectId, OpenMode.ForRead,true);
+                    transaction.Commit();
+                }
+            }
+            return entity;
+        }
+
+        /// <summary>
+        /// 根据实体范围设置当前视图
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <param name="scale">视图比例</param>
+        private void ViewEntity(Entity entity,double scale)
+        {
+            Database database = entity.Database;
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            using(ViewTableRecord viewTableRecord = new ViewTableRecord())
+            {
+                Point2d pointMin = new Point2d(entity.GeometricExtents.MinPoint.X, entity.GeometricExtents.MinPoint.Y);
+                Point2d pointMax = new Point2d(entity.GeometricExtents.MaxPoint.X, entity.GeometricExtents.MaxPoint.Y);
+                viewTableRecord.CenterPoint = new Point2d((pointMin.X+pointMax.X)/2, (pointMin.Y + pointMax.Y) / 2);
+                viewTableRecord.Width = Math.Abs(pointMax.X - pointMin.X)*scale;
+                viewTableRecord.Height = Math.Abs(pointMax.Y - pointMin.Y)*scale;
+                editor.SetCurrentView(viewTableRecord);
+            }
+        }
+    }
+}
